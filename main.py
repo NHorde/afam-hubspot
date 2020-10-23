24a0013cdfa7c2be23d405475df46ee7733cff83
Nicolas Horde <enhorde@gmail.com>
2020-10-23 15:18:31 -0400

    Small test

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 # Load environment
 load_dotenv()
 
-key = os.getenv("APIKEY")
+API_KEY = os.getenv("APIKEY")
 
-print(key)
+client = Hubspot3(api_key=API_KEY)
+all_companies = client.companies.get_all()
+
+print(all_companies)
