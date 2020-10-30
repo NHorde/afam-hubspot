36e0881a96ead24e462ec22115ca539cc006a7bb
Nicolas Horde <enhorde@gmail.com>
2020-10-30 15:28:38 -0400

    Refactoring

--- app/services/api/properties.py
+++ app/services/api/properties.py
@@ -1,27 +1,40 @@
+# Python
+import os
 
-API_KEY = os.getenv("API_KEY")
-API_URL = os.getenv("API_URL")
+# 3rd Party
+import requests
 
-headers = {}
+# Local
+from libs.state import State
+from libs.logger import BASE_LOGGER
+logger = BASE_LOGGER.getChild(__name__)
 
-url = f"{API_URL}/crm/v3/properties/Contact"
+def request_properties_api(state: State):
 
-querystring = {
-    "archived": "false",
-    "hapikey": API_KEY
-}
+    API_KEY = os.getenv("API_KEY")
 
-headers = {'accept': 'application/json'}
+    API_URL = os.getenv("API_URL")
 
-response = requests.request(
-    method="GET",
-    url=url,
-    headers=headers,
-    params=querystring
-)
+    headers = {}
 
-response = response.json()
+    url = f"{API_URL}/crm/v3/properties/Contact"
 
-# print(json.dumps(response, indent=3))
+    querystring = {
+        "archived": "false",
+        "hapikey": API_KEY
+    }
 
-print(response["results"][0])
+    headers = {'accept': 'application/json'}
+
+    response = requests.request(
+        method="GET",
+        url=url,
+        headers=headers,
+        params=querystring
+    )
+
+    response = response.json()
+
+    # print(json.dumps(response, indent=3))
+
+    print(response["results"][0])
--- app/services/manager.py
+++ app/services/manager.py
@@ -1,7 +1,14 @@
 # Local
 from libs.state import State
+from services.api.properties import request_properties_api
 from libs.logger import BASE_LOGGER
 logger = BASE_LOGGER.getChild(__name__)
 
+def manager_api(state: State):
+    request_properties_api(state=state)
+    logger.info("Done")
+
+
 def manager(state: State):
     logger.info("Hello")
+    manager_api(state=state)
