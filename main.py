a5c77b2f8ed714b7c49577f816c53a4b63403343
Nicolas Horde <enhorde@gmail.com>
2020-10-30 14:38:10 -0400

    Property API

--- main.py
+++ main.py
@@ -1,49 +1,40 @@
 # Local
 import os
+import json
 
-#3rd Party
+# 3rd Party
 from dotenv import load_dotenv
 import requests
+
 
 # Load environment
 load_dotenv()
 
-# https://pypi.org/project/hubspot-api-client/
-
 
 API_KEY = os.getenv("API_KEY")
-API_URL = f"{os.getenv('API_URL')}/crm/v3/objects/contacts"
+API_URL = os.getenv("API_URL")
 
+headers = {}
 
-properties = "email, firstname, lastname, alumni, donation, expertise, shasta_employer," \
-             " shasta_grant, shasta_mentor, shasta_intern"
+url = f"{API_URL}/crm/v3/properties/Contact"
 
 querystring = {
-    "limit": "100",
-    "properties": properties,
-    "paginateAssociations": "false",
     "archived": "false",
     "hapikey": API_KEY
 }
+
 headers = {'accept': 'application/json'}
 
-response = requests.request("GET", url=API_URL, headers=headers, params=querystring)
-print(response.text.encode('utf-8'))
+response = requests.request(
+    method="GET",
+    url=url,
+    headers=headers,
+    params=querystring
+)
 
-exit(1)
-# 2nd page
-querystring = {"after":"2606","limit":"100","properties":properties,"paginateAssociations":"false","archived":"false","hapikey":MY_HUBSPOT_API_KEY}
+response = response.json()
 
-response = requests.request("GET", url, headers=headers, params=querystring)
-response.encoding = "utf-8"
-print
-print(response.text.encode('utf-8'))
+print(json.dumps(response, indent=3))
 
-# Companies
-url = "https://api.hubapi.com/crm/v3/objects/companies"
-properties="name,domain,city,state,country,industry,phone_number,description"
-querystring = {"limit":"100","properties":properties,"paginateAssociations":"false","archived":"false","hapikey":MY_HUBSPOT_API_KEY}
-response = requests.request("GET", url, headers=headers, params=querystring)
-response.encoding = "utf-8"
-print
-print(response.text.encode('utf-8'))
+
+
